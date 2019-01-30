d59bcb6124e426d2f0247a4dacbc80a19571e9ab
deepak19mca <deepakbaghel794@gmail.com>
2019-01-30 15:35:19 +0530

    add ifelseifelse and login file

---
+++ ifelseifelse.php
@@ -0,0 +1,24 @@
+<?php
+
+$a=20;
+$b=30;
+$c=40;
+$d=20;
+
+if($a==$b)
+{
+	echo "a is equal to b<br/>";
+}
+else if($a==$c)
+{
+	echo "a is equal to c<br/>";
+}
+elseif ($a==$d)
+{
+	echo "a is equal to d";
+}
+else
+{
+	echo "any values are not same";
+}
+?>
---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login form</title>
+</head>
+<body>
+
+	<h1>
+		This is login form
+	</h1>
+
+</body>
+</html>
